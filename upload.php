8b319befd31651e7ac7650886ec8bbc99aa3ee33
BenderBanding <normpo4ta@gmail.com>
2016-02-15 15:11:33 +0200

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,14 @@
+<?php
+$data = array(); 
+$files = array();
+$uploaddir = './images/'; // . - текущая папка где находится submit.php
+// Создадим папку если её нет 
+if( ! is_dir( $uploaddir ) ) mkdir( $uploaddir, 0777 );
+// переместим файлы из временной директории в указанную
+foreach( $_FILES as $file ){
+	if( move_uploaded_file( $file['tmp_name'], $uploaddir . basename($file['name']) ) ){
+        $files[] = realpath( $uploaddir . $file['name'] );
+    } 
+    $data = $error ? array('error' => 'Ошибка загрузки файлов.') : array('files' => $files );
+    echo json_encode( $data );
+} 
